59492d56c67a9752ace8f717b855892a04d69136
Ларионов Денис Александрович <dlarionov@n-core.ru>
2017-04-24 13:43:46 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## README
+
+* https://www.rabbitmq.com/tutorials/tutorial-one-go.html
+* https://github.com/alco/gostart#faq3
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import {
+  "fmt"
+
+}
+
+func load_settings()  {
+
+}
+
+func start_listen_queue()  {
+
+}
+
+// /////////////////// MAIN ///////////////
+
+func main() {
+  fmt.Printf("Starting App...\n")
+
+  fmt.Printf("Start load settings\n")
+  load_settings()
+  fmt.Printf("End load settings\n")
+
+  fmt.Printf("Start load settings\n")
+  start_listen_queue()
+  fmt.Printf("End load settings\n")
+}
